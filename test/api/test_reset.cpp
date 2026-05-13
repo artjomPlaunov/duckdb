318d877de154b6cc50e4b06bdb9560077c5c8e2a
Artjom Plaunov <artyemnyc@gmail.com>
2026-05-13 14:36:07 +0200

    backport 22542 - adaptive transient segment allocation

--- test/api/test_reset.cpp
+++ test/api/test_reset.cpp
@@ -134,7 +134,9 @@ OptionValueSet GetValueForOption(const string &name, const LogicalType &type) {
 	    {"pin_threads", {"off"}},
 	    {"current_transaction_invalidation_policy", {"ALL_ERRORS_INVALIDATE_TRANSACTION"}},
 	    {"checkpoint_on_detach", {"ENABLED"}},
-	    {"enable_caching_operators", {false}}};
+	    {"enable_caching_operators", {false}},
+	    {"parallelize_sequential_sources", {false}},
+	    {"initial_column_segment_size", {4096}}};
 	// Every option that's not excluded has to be part of this map
 	if (!value_map.count(name)) {
 		switch (type.id()) {
